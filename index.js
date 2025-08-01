ba8c04f80f9e75c495a48d15a1c5f4dc9012298c
alex <alex@keicreations.nl>
2025-08-01 17:36:44 +0200

    v1.0.4

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ module.exports = {
         {
             "files": ["**/middleware/*.ts"],
             "rules": {
-                "consistent-return": "off",
+                "consistent-return": "off"
             }
         },
         {
